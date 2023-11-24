b8fa86f1bb1eb67255f7876e7eb117d5aebee8f0
daviv <1073113@gmail.com>
2023-11-24 10:57:56 +0800

    fex: remove image logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import logo from '@/assets/images/logo.png'
 import { Button } from '@/components/ui/button'
 import { auth } from '@clerk/nextjs'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+
 export default function Home() {
   // Redirect if logged-in
   const { userId } = auth()
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="flex h-screen flex-col items-center justify-center gap-5">
       {/*Group*/}
       <div className="flex items-center gap-4">
-        <Image src={logo} alt="logo" width={100} height={100} />
+        {/*<Image src={logo} alt="logo" width={100} height={100} />*/}
         <span className="text-4xl font-extrabold -tracking-tight lg:text-5xl">
           Note Taker
         </span>
